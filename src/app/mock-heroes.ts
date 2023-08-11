4e4c2e18e127071e02e104b4cc964edd17747397
KamalePragati1977 <141109450+KamalePragati1977@users.noreply.github.com>
2023-08-11 13:47:18 +0600

    2 files modified

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES:Hero[]= [
-  { id:1, name:'Sidhu Moose wala'},
-  { id:2, name:'Raftaar'},
-  { id:3, name:'Ikka'},
-  { id:4, name:'Abhishek malhan'},
-  { id:5, name:'Shubh'},
-  { id:6, name:'A P Dhillon'},
-  { id:7, name:'Honey Singh'},
-  { id:8, name:'Elvish Yadav'},
-  { id:9, name:'Badshah'}
+  { id:1, name:' Sidhu Moose wala '},
+  { id:2, name:' Raftaar '},
+  { id:3, name:' Ikka '},
+  { id:4, name:' Abhishek malhan '},
+  { id:5, name:' Shubh '},
+  { id:6, name:' A P Dhillon '},
+  { id:7, name:' Honey Singh '},
+  { id:8, name:' Elvish Yadav '},
+  { id:9, name:' Badshah '}
 ]
   
