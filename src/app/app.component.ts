583a829fcdeb6ccef543624782ad20b173d135d4
KamalePragati1977 <141109450+KamalePragati1977@users.noreply.github.com>
2023-08-12 10:08:17 +0600

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,14 @@ import { HEROES } from './mock-heroes';
   selector: 'app-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeroesComponent, CommonModule,HeroDetailComponent],
+  imports: [HeroesComponent, CommonModule, HeroDetailComponent],
 })
 export class AppComponent implements OnInit {
   // user:string="Manyaa";
   // mobileno:string="9579232080";
   // age:number=18;
   // avatar:string="https://e0.pxfuel.com/wallpapers/61/978/desktop-wallpaper-cute-baby-cat-cute-cat-kitten-thumbnail.jpg"
-  title = 'Tour of Heroes';
+  title = ' Tour of Heroes ';
 
   // heroes = HEROES;
 
